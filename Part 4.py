798f61a4243c89bd848e08a5529107f2b7dc60d2
scrollxwho <108821984+scrollxwho@users.noreply.github.com>
2022-10-02 15:45:46 +0300

    Add files via upload

--- Part 4.py
+++ Part 4.py
@@ -1,5 +1,4 @@
 from pygame import *
-#список уровня
 level = [
     "                                       ",
     "----------                    ---------",
@@ -14,10 +13,10 @@ level = [
     "                  o                    ",
     "---------------------------------------"]
 
-level_width  = len(level[0])*40 # прораховуємо ширину рівня
+level_width = len(level[0])*40  # прораховуємо ширину рівня
 level_height = len(level)*40    # прораховуємо висоту рівня
 
-# Главный класс
+
 class Settings(sprite.Sprite):
     def __init__(self, x, y, w, h, speed, img):
         super().__init__()
@@ -25,14 +24,16 @@ class Settings(sprite.Sprite):
         self.speed = speed
         self.width = w
         self.height = h
-        self.image = transform.scale(image.load(img), (self.width, self.height))
+        self.image = transform.scale(
+            image.load(img), (self.width, self.height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    def reset(self): 
+
+    def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-# Класс кнопок
+
 class Button():
     def __init__(self, color, x, y, w, h, text, fsize, txt_color):
 
@@ -58,7 +59,7 @@ class Button():
         window.blit(self.txt_image, (self.rect.x +
                     shift_x, self.rect.y + shift_y))
 
-# Класс врагов
+
 class Enemy(Settings):
     def __init__(self, x, y, w, h, speed, img, side):
         Settings.__init__(self, x, y, w, h, speed, img)
@@ -73,7 +74,7 @@ class Enemy(Settings):
         if self.side == "left":
             self.rect.x += self.speed
 
-# Класс главный герой 
+
 class Player(Settings):
 
     def r_l(self):
@@ -102,7 +103,8 @@ class Player(Settings):
             self.rect -= self.speed
         if keys[K_s]:
             self.rect += self.speed
-# Класс камера и функция к ней
+
+
 class Camera(object):
 
     def __init__(self, camera_func, width, height):
@@ -116,59 +118,92 @@ class Camera(object):
         self.state = self.camera_func(self.state, target.rect)
 
 
-    def camera_configure(camera, target_rect):
+def camera_configure(camera, target_rect):
 
-        l, t, _, _ = target_rect
-        _, _, w, h = camera
-        l, t = -l + win_width / 2, -t + win_height / 2
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l + win_width / 2, -t + win_height / 2
 
-        l = min(0, l)
-        l = max(-(camera.width - win_width), l)
-        t = max(-(camera.height - win_height), t)
-        t = min(0, t)
+    l = min(0, l)
+    l = max(-(camera.width - win_width), l)
+    t = max(-(camera.height - win_height), t)
+    t = min(0, t)
 
-    return Rect(1, t, w, h)
-#  Музыка
-mixer.init()
-fire_s = mixer.Sound('sounds/fire.ogg')
-kick = mixer.Sound('sounds/kick.ogg')
-k_up = mixer.Sound('sounds/k_coll.ogg')
-c_coll = mixer.Sound('sounds/c_coll.ogg')
-d_o = mixer.Sound('sounds/lock.ogg')
-tp = mixer.Sound('sounds/teleport.ogg')
-click = mixer.Sound('sounds/click.ogg')
-cst_o = mixer.Sound('sounds/chest.ogg')
-            
+    return Rect(l, t, w, h)
 
-# Создание окна
+
+# mixer.init()
+# fire_s = mixer.Sound('sounds/fire.ogg')
+# kick = mixer.Sound('sounds/kick.ogg')
+# k_up = mixer.Sound('sounds/k_coll.ogg')
+# c_coll = mixer.Sound('sounds/c_coll.ogg')
+# d_o = mixer.Sound('sounds/lock.ogg')
+# tp = mixer.Sound('sounds/teleport.ogg')
+# click = mixer.Sound('sounds/click.ogg')
+# cst_o = mixer.Sound('sounds/chest.ogg')
+
+c_count = 0
 win_width = 1280
 win_height = 720
 window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load("images/Fone.png"), (win_width, win_height))
+background = transform.scale(image.load(
+    "images/Fone.png"), (win_width, win_height))
 window.blit(background, (0, 0))
 
 display.set_caption("Maze_defection")
 
 clock = time.Clock()
-game = True
 FPS = 60
-# добавляем текст в игре
 
-# Переменные картинок
-hero_r = "images/sprite1_r.png"#1
-hero_l = "images/sprite1.png"#1
-enemy_l = "images/cyborg.png"#1
-enemy_r = "images/cyborg_r.png"#0
-coin = "images/coin.png"#1
-door_img = "images/door.png"#0
-key_img = "images/key.png"#0
-chest_open = "images/cst_open.png"#0
-chest_close = "images/cst_close.png"#0
-stair = "images/stair.png"#0
-port = "images/portal.png"#0
-platform = "images/platform.png"#1
-nothing = "images/nothing.png"#0
-power = "images/mana.png"#0
+hero_r = "images/sprite1_r.png"  # 1
+hero_l = "images/sprite1.png"  # 1
+enemy_l = "images/cyborg.png"  # 1
+enemy_r = "images/cyborg_r.png"  # 1
+coin = "images/coin.png"  # 1
+door_img = "images/door.png"  # 1
+key_img = "images/key.png"  # 1
+chest_open = "images/cst_open.png"  # 1
+chest_close = "images/cst_close.png"  # 1
+stair = "images/stair.png"  # 1
+port = "images/portal.png"  # 1
+platform = "images/platform.png"  # 1
+# nothing = "images/nothing.png"  # 0
+power = "images/mana.png"  # 1
+
+font.init()
+font1 = font.SysFont((' font / ariblk.ttf '), 200)
+gname = font1.render(' Blockada ', True, (106, 90, 205), (250, 235, 215))
+font2 = font.SysFont((' font / ariblk.ttf '), 60)
+e_tap = font2.render(' press ( e ) ', True, (255, 80, 255))
+k_need = font2.render(' You need a key to open ! ', True, (255, 0, 255))
+space = font2.render(' press( space ) to kill the enemy ', True, (255, 0, 255))
+font3 = font.SysFont((' font / calibrib.ttf '), 45)
+wasd_b = font3.render(
+    ' WASD - move buttons . You can only go up and down the stairs ', True, (255, 0, 0))
+space_b = font3.render(
+    ' Space - shoot button . You are a wizard who only knows one spell ', True, (255, 0, 0))
+e_b = font3.render(
+    ' E interaction button . Open doors , collect keys , activate portals ', True, (255, 0, 0))
+font4 = font.SysFont((' font / ariblk.ttf '), 150)
+done = font4.render('LEVEL DONE !', True, (0, 255, 0), (255, 100, 0))
+lose = font4.render(' YOU LOSE ! ', True, (255, 0, 0), (245, 222, 179))
+pausa = font4.render(' PAUSE ', True, (255, 0, 0), (245, 222, 179))
+
+btn_start = Button((178, 34, 34), 470, 300, 280, 70,
+                   ' START GAME ', 50, (255, 255, 255))
+btn_control = Button((178, 34, 34), 470, 450, 280, 70,
+                     ' HOW TO PLAY ', 50, (255, 255, 255))
+btn_exit = Button((178, 34, 34), 470, 600, 280, 70,
+                  ' EXIT GAME ', 50, (255, 255, 255))
+btn_menu = Button((178, 34, 34), 470, 600, 280, 70,
+                  ' BACK to MENU ', 50, (255, 255, 255))
+btn_restart = Button((178, 34, 34), 470, 450, 280, 70,
+                     ' RESTART ', 50, (255, 255, 255))
+btn_continue = Button((178, 34, 34), 470, 350, 280, 70,
+                      ' CONTINUE ', 50, (255, 255, 255))
+btn_pause = Button((178, 34, 34), 1200, 15, 50, 50,
+                   ' I I ', 40, (255, 255, 255))
+
 
 hero = Player(300, 650, 50, 50, 5, hero_l)
 
@@ -181,10 +216,8 @@ key2 = Settings(1500, 350, 50, 20, 0, key_img)
 portal = Settings(2700, 600, 100, 100, 0, port)
 chest = Settings(450, 130, 80, 80, 0, chest_close)
 
-# camera = Camera(camera_configure, level_width, level_height)
-# создаем кнопки
+camera = Camera(camera_configure, level_width, level_height)
 
-# Создание списков и групп спрайтов (перенести в функцию перезапуска
 blocks_r = []
 blocks_l = []
 coins = []
@@ -192,7 +225,22 @@ stairs = []
 platforms = []
 
 items = sprite.Group()
-# Рисуем уровень (перенести в функцию перезапуска
+
+items.add(door)
+items.add(key1)
+items.add(key2)
+items.add(portal)
+items.add(chest)
+items.add(en1)
+items.add(en2)
+items.add(hero)
+
+game = True
+
+k_door = False
+k_chest = False
+o_chest = False
+
 x = y = 0
 for r in level:
 
@@ -235,47 +283,77 @@ for r in level:
 
     y += 40
     х = 0
-# Функция перезапуска игры
-# Создание спрайтов (перенести в функцию перезапуска
-# Добавляем спрайты в группу
+
 while game:
+
     time.delay(15)
     window.blit(background, (0, 0))
     keys = key.get_pressed()
+
     for e in event.get():
         if e.type == QUIT:
             game = False
+
     en1.update()
     en2.update()
 
     hero.r_l()
-    
+
     for s in stairs:
-    if sprite.collide_rect(hero, s):
-        hero.u_d()
-        if hero.rect.y <= (s.rect.y - 40):
-            hero.rect.y = s.rect.y 40
-        if hero.rect.y >= (s.rect.y + 130):
-            hero.rect.y = s.rect.y + 130        
-    
-    #взаимодействие с сундуком,порталом,камера
+        if sprite.collide_rect(hero, s):
+            hero.u_d()
+            if hero.rect.y <= (s.rect.y - 40):
+                hero.rect.y = s.rect.y - 40
+            if hero.rect.y >= (s.rect.y + 130):
+                hero.rect.y = s.rect.y + 130
+
+    for r in blocks_r:
+        if sprite.collide_rect(hero, r):
+            hero.rect.x = r.rect.x + hero.width
+        if sprite.collide_rect(en1, r):
+            en1.side = ' left '
+            en1.image = transform.scale(
+                image.load(enemy_l), (en1.width, en1.height))
+        if sprite.collide_rect(en2, r):
+            en2.side = ' left '
+            en2.image = transform.scale(
+                image.load(enemy_l), (en2.width, en2.height))
+
+    for l in blocks_l:
+        if sprite.collide_rect(hero, l):
+            hero.rect.x = l.rect.x - hero.width
+        if sprite.collide_rect(en1, l):
+            en1.side = ' right '
+            en1.image = transform.scale(
+                image.load(enemy_r), (en1.width, en1.height))
+        if sprite.collide_rect(en2, l):
+            en2.side = ' right '
+            en2.image = transform.scale(
+                image.load(enemy_r), (en2.width, en2.height))
+
     if sprite.collide_rect(hero, chest) and k_chest == False:
-        win.blit(k_need, (450, 50))
-    if sprite.collide_rect(hero, chest) and k_chest == True and c_count !=15:
-        win.blit(e_tap, (450, 50))
+        window.blit(k_need, (450, 50))
+    if sprite.collide_rect(hero, chest) and k_chest == True and c_count != 15:
+
+        window.blit(e_tap, (450, 50))
+
         if keys[K_e]:
+
             o_chest = True
             c_count += 10
-            chest.image = transform.scale(image.load(chest_open),(chest.width, chest.height))
+            chest.image = transform.scale(image.load(
+                chest_open), (chest.width, chest.height))
             cst_o.play()
             k_door = True
+
     if sprite.collide_rect(hero, portal):
         tp.play()
         game = False
+
     camera.update(hero)
+
     for i in items:
-        win.blit(i.image, camera.apply(i))
-    
+        window.blit(i.image, camera.apply(i))
 
     if sprite.collide_rect(hero, key1):
         window.blit(e_tap, (500, 50))
@@ -292,11 +370,16 @@ while game:
             items.remove(key2)
             k_up.play()
 
-    # взаимодействие с обьектом бок Артем
-    # калькулятор собранных монет Нестеренко Влад
-    coin_c = font2.render(':' + str(c_count), True, (255, 255, 255))
-    win.bilt(transform.scale(image.load("images/coin.png"), (50,50)), (10, 10))
-    win.bilt(coin_c, (55, 15))
+    if sprite.collide_rect(hero, door) and k_door == False:
+        window.blit(k_need, (450, 50))
+        hero.rect.x = door.rect.x - 47
+    if sprite.collide_rect(hero, door) and k_door == True:
+        hero.rect.x = door.rect.x - 47
+        window.blit(e_tap, (500, 50))
+        if keys[K_e]:
+            door.rect.x += 1500
+            d_o.play()
+            k_door = False
 
     for c in coins:
         if sprite.collide_rect(hero, c):
@@ -304,17 +387,11 @@ while game:
             c_count += 1
             coins.remove(c)
             items.remove(c)
-    #поднимаемся по лестнице
-    #Собираем ключи
-    #открываем двери Миша
-    # открываем сундук 
-    # касание портала
-    # Удаляем врагов
-    
+
+    coin_c = font2.render(':' + str(c_count), True, (255, 255, 255))
+    window.blit(transform.scale(image.load(
+        "images/coin.png"), (50, 50)), (10, 10))
+    window.blit(coin_c, (55, 15))
+
     display.update()
-    clock.tick(FPS)            
-#Функция меню
-# Правила игры
-# Пауза в игре
-# Перезапуск игры
-# Уровень пройден
+    clock.tick(FPS)
