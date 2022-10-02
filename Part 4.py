698b8127290cacd497dccf05fb7f90ebcf145f21
Zhenya6 <112572866+Zhenya6@users.noreply.github.com>
2022-10-02 16:42:05 +0300

    Update Part 4.py
    class Mana, строка кода 77-91

--- Part 4.py
+++ Part 4.py
@@ -74,7 +74,22 @@ class Enemy(Settings):
         if self.side == "left":
             self.rect.x += self.speed
 
+class Mana(Settings):
+    def __init__(self, x, y, w, h, speed, img, side):
+        Settings.__init__(self, x, y, w, h, speed, img)
+        
+        self.side = side
+        
+    def update(self):
+        global side
+        
+        if self.side == 'left':
+            self.rect.x -= self.speed
+        if self.side == 'right':
+            self.rect.x += self.speed
 
+mana = Mana(0, -100, 25, 25, 35, power, 'left')
+            
 class Player(Settings):
 
     def r_l(self):
