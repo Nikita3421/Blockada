428c51da0bfabfc9ea00593921574d547e48ea72
Zhenya6 <112572866+Zhenya6@users.noreply.github.com>
2022-10-16 17:11:38 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -22,3 +22,83 @@ for l in blocks_1:
         if sprite.collide_rect(e4, l):
             en4.side = 'right'
             en4.image = transform.scale(image.load(enemy_r),(en4.width, en4.height))
+
+def rules(): #правила
+    rule = True
+    mixer.music.stop()
+
+    while rule:
+
+        for e in event.get():
+            if e.type == QUIT:
+                rule = False
+        
+        time.delay(15)
+
+        win.blit(bg, (0, 0))
+        win.blit(gname,(320, 70))
+        win.blit(wasd_b,(50, 250))
+        win.blit(space_b,(50, 350))
+        win.blit(e_b,(50, 450))
+
+        btn_menu.draw(0, 5)
+
+        pos_x, pos_y = mouse.get_pos()
+
+        for e in event.get():
+
+            if btn_menu.rect.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                rule = False
+                menu()
+            
+            if e.type == QUIT:
+                rule = False
+            
+        display.update()
+
+def restart(): #перезапуск
+
+    over = True
+
+    mixer.music.stop()
+    mixer.music.load('sounds/game_over.ogg')
+    mixer.music.play()
+
+    while over:
+
+        for e in event.get():
+            if e.type == QUIT:
+                over = False
+
+        time.delay(15)
+
+        win.fill((0,0,0))
+        win.blit(lose,(350, 200))
+
+        btn_restart.draw(60, 5)
+        btn_menu.draw(0, 5)
+
+        pos_x, pos_y = mouse.get_pos()
+
+        for e in event.get():
+
+            if btn_restart.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                mixer.music.stop()
+                over = False
+                res_pos()
+                lvl_1()
+
+            if btn_restart.collidepoint((pos_x, pos_y)) and e.type == MOUSEBUTTONDOWN:
+                click.play()
+                over = False
+                menu()
+
+        display.update()
+
+def res_pos():
+
+    global items, manas, coins, platforms, stairs, blocks_r, blocks_l
+    global hero, en1, en2, en3, en4, door, key1, key2, portal, chest, camera
+    global k_door, k_chest, o_chest, c_count
